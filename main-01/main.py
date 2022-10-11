1e40aecb0c99992eda5b3d448884c6d77020154f
Maksydenko <maksydenko@gmail.com>
2022-10-11 15:50:14 +0300

    Update main.py

--- main-01/main.py
+++ main-01/main.py
@@ -8,15 +8,6 @@ class Rational:
         self.denominator = denominator
 
 
-    def get_float(self):
-        return self.numberator / self.denominator
-
-
-    def __str__(self):
-        return f"{self.numberator}/{self.denominator};" \
-               f"float {self.get_float()}"
-
-
     @staticmethod
     def reduce_fraction(numberator, denomunator):
         from math import gcd
@@ -27,9 +18,25 @@ class Rational:
         return numberator, denomunator
 
 
+    def get_float(self):
+        return self.numberator / self.denominator
+
+
+    def __str__(self):
+        return f"{self.numberator}/{self.denominator};" \
+               f"float {self.get_float()}"
+
+
 # Task 2
 
 class Rectangle:
+
+    def __init__(self, width=80, height=60):
+        if width or height <= 0:
+            raise ValueError("Side cannot be equal to or less than 0")
+        self.width = Rectangle.limit_side(width)
+        self.height = Rectangle.limit_side(height)
+
     max_size = 100
 
     @classmethod
@@ -37,13 +44,6 @@ class Rectangle:
         if side > cls.max_size:
             return cls.max_size
         return side
-
-
-    def __init__(self, width=80, height=60):
-        if width or height <= 0:
-            raise ValueError("Side cannot be equal to or less than 0")
-        self.width = Rectangle.limit_side(width)
-        self.height = Rectangle.limit_side(height)
 
 
     def get_perimeter(self):
