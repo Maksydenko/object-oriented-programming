989e53cf6663f39f020e990de6c9ea1799ef60b2
Maksydenko <maksydenko@gmail.com>
2022-10-11 15:32:29 +0300

    Create main.py

---
+++ additional-01/main.py
@@ -0,0 +1,21 @@
+class Book:
+
+    def __init__(self, book_value, pages_number):
+        self.book_value = book_value
+        self.pages_number = pages_number
+
+
+    def __add_pages(self):
+        return self.pages_number + 10
+
+
+    def __get_price(self):
+        if self.__add_pages() > 200:
+            return self.book_value / 2
+        return self.book_value
+
+
+    def __str__(self):
+        if self.book_value or self.pages_number < 0:
+            raise ValueError("Wrong value of book or number of pages!")
+        return f"Value of book: ${self.__get_price()}"
