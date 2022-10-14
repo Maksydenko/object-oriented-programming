c24ec27591ee45e0ca9cf3a78254989b3f4c4f05
Maksydenko <maksydenko@gmail.com>
2022-10-14 19:26:04 +0300

    Update main.py

--- control-01/main.py
+++ control-01/main.py
@@ -46,20 +46,21 @@ class Weather:
 
 class Date:
 
-    def __init__(self, day_date, month=0, year=0):
-        self.day_date = day_date
-        self.month = month
+    def __init__(self, month_date, day=0, year=0):
+        self.month_date = month_date
+        self.day = day
         self.year = year
 
 
     def change_format(self):
         number_date = "%d.%m.%Y"
-        word_date = "%d %B %Y"
+        word_date = "%B %d, %Y"
 
-        if self.month == 0 and self.year == 0:
-            date = datetime.strptime(self.day_date, number_date)
+        if self.day == 0 and self.year == 0:
+            date = datetime.strptime(self.month_date, number_date)
             return date.strftime(word_date)
-        string_date = str(self.day_date) + " " + self.month + " " + str(self.year)
+        string_date = self.month_date + " " + str(self.day) \
+                      + ", " + str(self.year)
         date = datetime.strptime(string_date, word_date)
         return date.strftime(number_date)
 
