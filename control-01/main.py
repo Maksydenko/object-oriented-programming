9dbf50353461a6855eab05d4187357efb6a182ff
Maksydenko <maksydenko@gmail.com>
2022-10-14 12:09:01 +0300

    Update main.py

--- control-01/main.py
+++ control-01/main.py
@@ -17,6 +17,7 @@ class Weather:
         days = Weather.list_days
         number_day = 0
         max_pressure = 0
+        
         for count_day in range(len(days)):
             if days[count_day][2] > max_pressure:
                 max_pressure = days[count_day][2]
@@ -26,8 +27,8 @@ class Weather:
 
     def __str__(self):
         return f"{self.check_pressure()[0]} temperature " \
-               f"{self.check_pressure()[1]} degrees Celsius, atmospheric " \
-               f"pressure {self.check_pressure()[2]} mmHg, " \
+               f"{self.check_pressure()[1]} degrees Celsius, " \
+               f"atmospheric pressure {self.check_pressure()[2]} mmHg, " \
                f"chance of precipitation {self.check_pressure()[3]}%"
 
 
@@ -35,11 +36,21 @@ class Weather:
 
 class Date:
 
-    def get_date(self):
+    def __init__(self, day, month=0, year=0):
+        self.day = day
+        self.month = month
+        self.year = year
+
+
+    def change_format(self):
         from datetime import datetime, date
 
-        return date.today()
+        if self.month == 0 or self.year == 0:
+            date = datetime.strptime(self.day, "%d.%m.%Y")
+            return date.strftime("%d %B %Y")
+        date = datetime.strptime(str(self.day) + " " + self.month + " " + str(self.year), "%d %B %Y")
+        return date.strftime("%d.%m.%Y")
 
 
-    def write_date(self):
-        return self.get_date().strftime("%d %B %Y")
+    def __str__(self):
+        return f"{self.change_format()}"
